8676f13099e194dfcfbc8a189cb1eed96cdc2376
Gianluca Acerbis <acerbisgianluca@gmail.com>
2019-01-31 21:43:00 +0100

    Updated cron execution every 30 secs
    Added eslint config and removed config files with authentication data

--- server/cron.js
+++ server/cron.js
@@ -1,45 +1,37 @@
-var cron = require('node-cron');
-var Area = require('./data/Area');
-var gpio = require('gpio');
+let cron = require('node-cron');
+let Area = require('./models/Area');
+let client = require('./mqtt');
 
-var gpio19 = gpio.export(19, { direction: 'out', ready: function() {} });
-var gpio16 = gpio.export(16, { direction: 'out', ready: function() {} });
-var gpio26 = gpio.export(26, { direction: 'out', ready: function() {} });
-var gpio20 = gpio.export(20, { direction: 'out', ready: function() {} });
-
-var relays = [gpio19, gpio16, gpio26, gpio20];
-
-relays.forEach(function(el, i) {
-	relays[i].set(0);
+let areasStatus = [],
+	update;
+Area.find().exec((err, areas) => {
+	if (err) return;
+	areas.forEach((el, i) => {
+		areasStatus[i] = 0;
+	});
 });
 
-cron.schedule('*/10 * * * * *', function() {
-	Area.find().exec(function(err, areas) {
-		areas.forEach(function(el, i) {
-			if (el.acceso && relays[i].value == 0) {
+cron.schedule('*/30 * * * * *', () => {
+	Area.find().exec((err, areas) => {
+		if (err) return;
+		areas.forEach((el, i) => {
+			if (el.acceso && areasStatus[i] === 0) {
 				console.log(i + ' - ACCESO');
-				relays[i].set();
-			} else if (!el.acceso && relays[i].value == 1) {
+				areasStatus[i] = 1;
+				update = { status: 'on' };
+				client.publish(
+					'arduinostatus' + el.nome,
+					JSON.stringify(update)
+				);
+			} else if (!el.acceso && areasStatus[i] === 1) {
 				console.log(i + ' - SPENTO');
-				relays[i].set(0);
+				areasStatus[i] = 0;
+				update = { status: 'off' };
+				client.publish(
+					'arduinostatus' + el.nome,
+					JSON.stringify(update)
+				);
 			}
 		});
 	});
 });
-
-// function write(pin) {
-//     gpio.write(pin, false, function(err) {
-//         if (err) throw err;
-//     });
-
-/*
-ZONE: A - B - STUFA1 - STUFA2
-PIN: 19 - 26 - 16 - 20
-
-ASSOCIATION:{
-A - 19
-B -
-STUFA1 -
-
-}
-*/
