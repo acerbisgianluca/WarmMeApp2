239117fed018192e437f74b84c8620504104b0c1
Gianluca Acerbis <acerbisgianluca@gmail.com>
2019-02-02 17:20:16 +0100

    Git is broken

---
+++ arduino/readData.py
@@ -0,0 +1,31 @@
+import serial
+import paho.mqtt.client as mqtt
+import ssl
+import time
+import configparser
+
+
+def connected(client, userdata, flags, rc):
+    print("Connesso al broker")
+
+
+# Carico la configurazione
+config = configparser.ConfigParser()
+config.read('config.ini')
+# Creo il client MQTT, imposto l'utente e TLS e mi connetto
+client = mqtt.Client("arduino")
+client.username_pw_set(
+    config["MQTT"]["username"], password=config["MQTT"]["password"])
+client.tls_set_context(ssl.create_default_context())
+client.connect("mqtt.acerbisgianluca.com", port=8883)
+client.on_connect = connected
+client.loop_start()
+
+# Creo l'oggetto serial impostando la porta USB e i baud
+ser = serial.Serial('/dev/ttyACM0', 9600)
+while True:
+    if(ser.in_waiting > 0):
+        line = ser.readline().decode("utf-8").rstrip()
+        if("NAN" not in line and line.startswith("{\"")):
+            client.publish("arduinodata", line)
+            print(line)
